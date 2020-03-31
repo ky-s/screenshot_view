9a461da79afa5d7883126ecd33f2fdd69310c59a
ky-s <kyohei_86@hotmail.com>
2020-03-31 22:47:42 +0900

    [update] add List view

--- layouts/default.vue
+++ layouts/default.vue
@@ -100,17 +100,17 @@ export default {
           icon: 'mdi-apps',
           title: 'Welcome',
           to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+        }//,
+        // {
+        //   icon: 'mdi-chart-bubble',
+        //   title: 'Inspire',
+        //   to: '/inspire'
+        // }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Your Screenshots'
     }
   }
 }
